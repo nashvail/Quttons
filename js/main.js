0a8f0f5a46d22d9f816e679137d8831a662e4bfd
nashmaniac <nishantve1@gmail.com>
2015-06-07 11:26:26 +0530

    Working now with better config

--- js/main.js
+++ js/main.js
@@ -1,18 +1,9 @@
 'use strict';
 
-var materialDelete = MaterialButton.getInstance($('#deleteBox'));
-// Initalize the dialog with final States icon and easing
-materialDelete.init({
+var confirmButton = MaterialButton.getInstance($('#confirmButton'));
+confirmButton.init({
 	width : 70,
 	height : 70,
-	backgroundColor : "#EB1220",
-	icon : './images/icon_delete.png',
-	easing : 'easeInOutQuint'
-});
-
-
-// You can indepdently handle click events on buttons inside the dialog
-$('.confirm').click(function(event) {
-	console.log("Hey how are you doing");
-	event.stopPropagation();
+	icon : './images/icon_done.png',
+	backgroundColor : "#76CB00"
 });
